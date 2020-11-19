ce31221d565b4249c5ea01d46751201989cfb523
junhodori <junhodori@naver.com>
2020-11-19 18:17:52 +0900

    commit

--- src/pattern/behavioral/memento/Memento.java
+++ src/pattern/behavioral/memento/Memento.java
@@ -1,4 +1,13 @@
 package pattern.behavioral.memento;
 
 public class Memento {
+    private String state;
+
+    public Memento(String state) {
+        this.state = state;
+    }
+
+    public String getState() {
+        return state;
+    }
 }
--- src/pattern/behavioral/memento/MementoTest.java
+++ src/pattern/behavioral/memento/MementoTest.java
@@ -1,4 +1,35 @@
 package pattern.behavioral.memento;
 
 public class MementoTest {
+    public static void main(String[] args) {
+        /**
+         * 특정 시점에 객체의 상태 값을 저장한다
+         * Originator 상태가 저장이 되어야 할 대상 클래스이다
+         * CareTaker 특정 시점의 상태 값을 리스트로 보관 할 클래스이다
+         */
+        Originator originator = new Originator();
+        CareTaker careTaker = new CareTaker();
+
+        originator.setState("State #1");
+        originator.setState("State #2");
+        // caretaker 첫번째 저장
+        careTaker.add(originator.saveStateToMemento());
+
+        originator.setState("State #3");
+        // caretaker 두번째 저장
+        careTaker.add(originator.saveStateToMemento());
+
+        originator.setState("State #4");
+        System.out.println("Current State : " + originator.getState());
+
+        // caretaker 첫번째 로드
+        originator.getStateFromMemento(careTaker.get(0));
+        System.out.println("First saved State : " + originator.getState());
+
+        // caretaker 번째 로드
+        originator.getStateFromMemento(careTaker.get(1));
+        System.out.println("Second saved State : " + originator.getState());
+
+
+    }
 }
